e6d71e0d226dd171f3b054faf5446e862a40c904
Dora He <dora.shan.he@gmail.com>
2020-10-23 18:23:44 +1100

    -

--- run.py
+++ run.py
@@ -3,13 +3,13 @@ from scripts.output_results import write_batch_experiment_summary
 from datetime import date, datetime
 
 
-repeat_num = 1
+repeat_num = 5
 # household_nums = [2000, 4000, 6000, 8000, 10000]
 # household_nums = [20, 40, 60, 80, 100]
-household_nums = [50]
-care_factor_weights = [0, 1, 5, 10, 50, 100, 500, 1000, 5000, 10000]
-new_data = True
-# new_data = False
+household_nums = [5000]
+care_factor_weights = [0.001, 1, 5, 10, 50, 100, 500, 1000, 5000, 10000]
+# new_data = True
+new_data = False
 type_cost_function = "piece-wise"
 # type_cost_function = "linear"
 
@@ -31,11 +31,16 @@ group_by_columns = [k0_households_no, k0_tasks_no, "algorithm", k0_penalty_weigh
 
 
 def run():
-    for num_household in household_nums:
-        for cfw in care_factor_weights:
-            for r in range(repeat_num):
-                date_time_experiment_folder = date_time_folder + f"h{num_household}-t{no_tasks_min}-w{cfw}-r{r}/"
+    for r in range(repeat_num):
+        for num_household in household_nums:
+            for cfw in care_factor_weights:
 
+                if cfw == care_factor_weights[0]:
+                    new_data = True
+                else:
+                    new_data = False
+
+                date_time_experiment_folder = date_time_folder + f"h{num_household}-t{no_tasks_min}-w{cfw}-r{r}/"
                 experiment_summary = experiment(num_household, no_tasks_min, no_tasks_min + 2, cfw,
                                                 new_data, type_cost_function,
                                                 algorithms_labels, date_time_experiment_folder)
